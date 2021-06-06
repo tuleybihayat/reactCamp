bed53afd542b6807c2c9649276f3a951ba80713d
Emre Şahin | tuleybihayat <emre@tuleybihayat.net>
2021-06-06 11:58:05 +0300

    11. ders sonu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Dashboard from './layout/Dashboard';
 import 'semantic-ui-css/semantic.min.css'
+import { Container } from 'semantic-ui-react';
+import Navi from './layout/Navi';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard></Dashboard>
+      <Navi></Navi>
+      <Container className = "main">
+        <Dashboard>
+
+        </Dashboard>
+      </Container>
+
     </div>
   );
 }
